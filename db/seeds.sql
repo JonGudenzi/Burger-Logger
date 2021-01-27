45ae46b5be3cf1cd1214b52a3708195e887670c2
JonGudenzi <jongudenzi@gmail.com>
2021-01-26 22:58:47 -0500

    Created seeds data and ran DB in mysql

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('hamburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('mushroom burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon burger', false);
