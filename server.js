5ca9c0b7d280934c18f9d29d85ac9a676e35d81a
JonGudenzi <jongudenzi@gmail.com>
2021-01-28 18:07:14 -0500

    got index connection working on port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 4050;
 
 var app = express();
 
@@ -18,9 +18,9 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-// var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/burgerController.js");
 
-// app.use(routes);
+app.use(routes);
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
