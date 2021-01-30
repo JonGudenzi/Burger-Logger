1a3c4eeee2cf6d2cd3b46b4fb8e0aab4d93c547e
JonGudenzi <jongudenzi@gmail.com>
2021-01-30 14:12:31 -0500

    added JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection(
-  process.env.JAWSDB_URL || {
+  process.JAWSDB_URL || {
   host: "localhost",
   port: 3306,
   user: "root",
